c8cde0cfeea98c2bfec1b424f06904eeffd3c8e9
ToggoTheDirty <davemark.dongon@gmail.com>
2019-01-18 21:14:43 +0800

    Add files via upload
    updated doms online

---
+++ domsonline/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a012001432
+ * Date: 1/9/2019
+ * Time: 6:11 PM
+ */
+echo '<div align="right" class="ui-widget"> Todays is '. date("Y/m/d").' '.date("l").' Welcome </div>' ;
+
+require 'index.html';
+
+?>
---
+++ domsonline/php/ajax-search.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a012001432
+ * Date: 1/10/2019
+ * Time: 4:33 PM
+ */
+
+$strArray = array('ActionScript',
+    'AppleScript',
+    'Asp',
+    'BASIC',
+    'C',
+    'C++',
+    'Clojure',
+    'COBOL',
+    'ColdFusion',
+    'Erlang',
+    'Fortran',
+    'Groovy',
+    'Haskell',
+    'Java',
+    'JavaScript',
+    'Lisp',
+    'Perl',
+    'PHP',
+    'Python',
+    'Ruby',
+    'Scala',
+    'Scheme');
+
+
+
+print_r(implode(",",$strArray));
